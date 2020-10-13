4a85c36a7c8dca13401d7419a1dac46e6388ad8a
sevdaghalarova <51816139+sevdaghalarova@users.noreply.github.com>
2020-10-14 02:06:02 +0400

    fonksiyon icinde fonksiyon döndürmekte

--- ic_ice_fonksiyonlar.py
+++ ic_ice_fonksiyonlar.py
@@ -1,22 +1,18 @@
-def fonk(*args):
-    for i in args:
-        print(i)
-
-fonk(1,2,3,4,5,5,3,5,2,4)
-
-def fonk1(**kwargs): #sozluk seklinde olur
-    for i,j in kwargs.items():
-        print(i,j)
-
-fonk1(isim="sevda",soyisim="agha")
-
-
-def ic_ice(*args):
-    def toplama(args):
+def ana_fonksiyon(islem):
+    def toplama(*args):
         toplam=0
         for i in args:
             toplam+=i
         return toplam
-    print(toplama(args))
+    def carp(*args):
+        carpim=1
+        for i in args:
+            carpim*=i
+        return carpim
+    if islem=="toplama":
+        return toplama
+    else:
+        return carp
 
-ic_ice(1,2,3,4,5)
+fonk=ana_fonksiyon("carp")
+print(fonk(1,2,3,4,5))
